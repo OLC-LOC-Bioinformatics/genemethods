2c70472c50e7c0dd7c28e3b321c0f0bf7d903433
adamkoziol <adam.koziol@inspection.gc.ca>
2019-05-27 16:12:58 -0400

    Added typingclasses package from olctools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="genemethods",
-    version="0.0.0.3",
+    version="0.0.0.5",
     packages=find_packages(),
     author="Adam Koziol",
     author_email="adam.koziol@canada.ca",
