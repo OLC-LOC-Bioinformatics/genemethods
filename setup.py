cf775dc5a6444063b0c93341868d9bdd1b03e68d
adamkoziol <adam.koziol@inspection.gc.ca>
2019-05-30 10:31:29 -0400

    Adding travis CI

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="genemethods",
-    version="0.0.0.8",
+    version="0.0.0.10",
     packages=find_packages(),
+    include_package_data=True,
     author="Adam Koziol",
     author_email="adam.koziol@canada.ca",
     url="https://github.com/OLC-Bioinformatics/genemethods",
