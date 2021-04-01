cc47e5a18b6704b253975fae4fa8761d9836bee2
Adam Koziol <adam.koziol@canada.ca>
2021-04-01 11:20:49 -0400

    Added primer files to MANIFEST

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="genemethods",
-    version="0.0.0.46",
+    version="0.0.0.47",
     packages=find_packages(),
     include_package_data=True,
     author="Adam Koziol",
