97522cf8d98e104848ed9b1f10ae1405b8937669
Adam Koziol <adam.koziol@canada.ca>
2022-07-07 16:13:51 -0400

    Fixed issue with full contig extraction not working

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="genemethods",
-    version="0.0.0.58",
+    version="0.0.0.59",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
